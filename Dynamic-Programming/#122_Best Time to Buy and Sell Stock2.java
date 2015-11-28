32b32435f7bc4cc52baa9e8373cb8ba2fb038aa4
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2015-11-28 01:25:06 -0600

    Create #122_Best Time to Buy and Sell Stock2.java

---
+++ Dynamic-Programming/#122_Best Time to Buy and Sell Stock2.java
@@ -0,0 +1,17 @@
+//Say you have an array for which the ith element is the price of a given stock on day i.
+
+//Design an algorithm to find the maximum profit. 
+//You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). 
+//However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+//和日常生活中得买股票一样，只不过能预知未来~
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length<2) return 0;
+        int profit=0;
+        for (int i=0;i<prices.length-1;i++){
+            if(prices[i]<prices[i+1])
+                profit+=prices[i+1]-prices[i];
+        }
+        return Math.max(0,profit);
+    }
+}
