a6cba976d4de83b564afac311103051568c363d1
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2015-12-22 11:02:22 -0600

    Create Valid Palindrome.java

---
+++ Two-Pointers/Valid Palindrome.java
@@ -0,0 +1,33 @@
+//Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+//For example,
+//"A man, a plan, a canal: Panama" is a palindrome.
+//"race a car" is not a palindrome.
+
+//Note:
+//Have you consider that the string might be empty? This is a good question to ask during an interview.
+
+//For the purpose of this problem, we define empty string as valid palindrome.
+
+
+public class Solution {
+    public boolean isPalindrome(String s) {
+        int i=0;
+        int j=s.length()-1;
+        s=s.toUpperCase();
+        while(i<j){
+            while(i<j&&!(s.charAt(i)>='0'&&s.charAt(i)<='9'||s.charAt(i)<='Z'&&s.charAt(i)>='A')){
+                i++;
+            }
+            while(i<j&&!(s.charAt(j)>='0'&&s.charAt(j)<='9'||s.charAt(j)<='Z'&&s.charAt(j)>='A')){
+                j--;
+            }
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
