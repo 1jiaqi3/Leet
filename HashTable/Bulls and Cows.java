cb5ce28b53896f61567bbb1ad06d3ba265eed6ab
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2015-12-31 22:49:45 -0600

    Update Bulls and Cows.java

--- HashTable/Bulls and Cows.java
+++ HashTable/Bulls and Cows.java
@@ -10,7 +10,8 @@
 //Secret number:  "1807"
 //Friend's guess: "7810"
 //Hint: 1 bull and 3 cows. (The bull is 8, the cows are 0, 1 and 7.)
-//Write a function to return a hint according to the secret number and friend's guess, use A to indicate the bulls and B to indicate the cows. In the above example, your function should return "1A3B".
+//Write a function to return a hint according to the secret number and friend's guess, 
+//use A to indicate the bulls and B to indicate the cows. In the above example, your function should return "1A3B".
 
 //Please note that both secret number and friend's guess may contain duplicate digits, for example:
 
