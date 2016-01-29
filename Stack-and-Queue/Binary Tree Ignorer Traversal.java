86bc5908392bb3a0a34e12c4bb38487bcc69f620
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2016-01-28 18:38:33 -0600

    Create Binary Tree Ignorer Traversal.java

---
+++ Stack-and-Queue/Binary Tree Ignorer Traversal.java
@@ -0,0 +1,42 @@
+/*
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+For example:
+Given binary tree {1,#,2,3},
+   1
+    \
+     2
+    /
+   3
+return [1,3,2].
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ret = new ArrayList<>();
+        if (root == null) {
+            return ret;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.empty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            if (!stack.empty()) {
+                TreeNode cur = stack.pop();
+                ret.add(cur.val);
+                root = cur.right;
+            }
+        }
+        return ret;
+    }
+}
