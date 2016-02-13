e7d9b0a6572a19fe26dd33e5c4e606f448d10c52
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2016-02-12 21:31:55 -0600

    Update Merge Two Sorted Lists.java

--- LinkedList/Merge Two Sorted Lists.java
+++ LinkedList/Merge Two Sorted Lists.java
@@ -10,26 +10,22 @@
  */
 public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1==null) return l2;
-        if(l2==null) return l1;
-        ListNode dummyHead=new ListNode(0);
-        ListNode p=dummyHead;
-        while(l1!=null&&l2!=null){
-            if(l1.val<=l2.val){
-                p.next=l1;
-                l1=l1.next;
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = dummyHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            } else if (l2.val < l1.val) {
+                p.next = l2;
+                l2 = l2.next;
             }
-            else if(l2.val<l1.val){
-                p.next=l2;
-                l2=l2.next;
-            }
-            p=p.next;
+            p = p.next;
         }
-        if(l1==null){
-            p.next=l2;
-        }
-        else if(l2==null){
-            p.next=l1;
+        if (l1 == null) {
+            p.next = l2;
+        } else if (l2 == null) {
+            p.next = l1;
         }
         return dummyHead.next;
     }
