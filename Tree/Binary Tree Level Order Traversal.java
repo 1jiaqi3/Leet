481b698a8b79201ae14e71e4c731e543c8e23224
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2016-03-06 12:04:26 -0600

    Update Binary Tree Level Order Traversal.java

--- Tree/Binary Tree Level Order Traversal.java
+++ Tree/Binary Tree Level Order Traversal.java
@@ -24,38 +24,23 @@ return its level order traversal as:
  *     TreeNode(int x) { val = x; }
  * }
  */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 public class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        int level = height(root);
         List<List<Integer>> ret = new ArrayList<>();
-        for (int i = 0; i < level; i++) {
-            List<Integer> levelList = new ArrayList<>();
-            ret.add(levelList);
-        }
         traversal(ret, 0, root);
         return ret;
     }
     public void traversal(List<List<Integer>> ret, int level, TreeNode root){
-        if(root != null){
+        if (root == null) {
+            return;
+        } else {
+            if (ret.size() <= level) {
+                List<Integer> levelList = new ArrayList<>();
+                ret.add(levelList);
+            }
             ret.get(level).add(root.val);
             traversal(ret, level + 1, root.left);
             traversal(ret, level + 1, root.right);
-        }
-    }
-    public int height(TreeNode root) {
-        if(root == null) {
-            return 0;
-        } else {
-            return Math.max(height(root.left), height(root.right)) + 1;
         }
     }
 }
