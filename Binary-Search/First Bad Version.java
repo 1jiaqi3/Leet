5e52feff14ae8bd242687ad9b5f3c8ac06bb9543
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2016-03-07 14:27:26 -0600

    Update First Bad Version.java

--- Binary-Search/First Bad Version.java
+++ Binary-Search/First Bad Version.java
@@ -1,29 +1,25 @@
+/*
+You are a product manager and currently leading a team to develop a new product. 
+Unfortunately, the latest version of your product fails the quality check. 
+Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+*/
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
 
-//You are a product manager and currently leading a team to develop a new product. 
-//Unfortunately, the latest version of your product fails the quality check. 
-//Since each version is developed based on the previous version, all the versions after a bad version are also bad.
-
-//Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
-
-//You are given an API bool isBadVersion(version) which will return whether version is bad. 
-//Implement a function to find the first bad version. You should minimize the number of calls to the API.
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        long low=1;
-        long high=n;
-        int mid=(int)((low+high)/2);
-        while(low<=high){
-            if(isBadVersion(mid)){
-                high=mid-1;
-                mid=(int)((low+high)/2);
-            }
-            else{
-                low=mid+1;
-                mid=(int)((low+high)/2);
+        int low = 1;
+        int high = n;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (isBadVersion(mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
             }
         }
-        return (int)low;
+        return low;
     }
 }
