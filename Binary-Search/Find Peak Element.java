65c44ac7902c7fdbad145d84e42233e5df6a15ac
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2016-03-08 00:01:57 -0600

    Update Find Peak Element.java

--- Binary-Search/Find Peak Element.java
+++ Binary-Search/Find Peak Element.java
@@ -11,32 +11,17 @@
 //Your solution should be in logarithmic complexity.
 public class Solution {
     public int findPeakElement(int[] nums) {
-        int len=nums.length;
-        if(len==1) return 0;
-        int low=0;
-        int high=len-1;
-        int mid=(low+high)/2;
-        while((mid-1)>=0&&(mid+1)<len){
-            if(nums[mid]>nums[mid-1]&&nums[mid]>nums[mid+1])
-                return mid;
-            else if(nums[mid]<nums[mid-1]){
-                high=mid-1;
-                mid=(low+high)/2;
-            }
-            else if(nums[mid]<nums[mid+1]){
-                low=mid+1;
-                mid=(low+high)/2;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end) {
+            int mid1 = (start + end) / 2;
+            int mid2 = mid1 + 1;
+            if (nums[mid1] > nums[mid2]) {
+                end = mid1;
+            } else if (nums[mid1] < nums[mid2]) {
+                start = mid1 + 1;
             }
         }
-        if(mid==0){
-            if(nums[mid]>nums[mid+1])
-            return mid;
-            else return mid+1;
-        }
-        else{
-            if(nums[mid]>nums[mid-1])
-            return mid;
-            else return mid-1;
-        }
+        return start;
     }
 }
