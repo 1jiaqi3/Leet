5c6f9132a83b2560b617aae31f982ea2f32c6c61
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2016-03-08 00:43:53 -0600

    Update H-index II.java

--- Binary-Search/H-index II.java
+++ Binary-Search/H-index II.java
@@ -7,19 +7,18 @@ Expected runtime complexity is in O(log n) and the input is sorted.
 */
 public class Solution {
     public int hIndex(int[] citations) {
-        int i = 0;
-        int j = citations.length - 1;
-        int mid = (i + j) / 2;
-        while (i <= j) {
-            mid = (i + j) / 2;
+        int start = 0;
+        int end = citations.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
             if (citations[mid] == citations.length - mid) {
                 return citations[mid];
             } else if (citations[mid] < citations.length - mid) {
-                i = mid + 1;
+                start = mid + 1;
             } else {
-                j = mid - 1;
+                end = mid - 1;
             }
         }
-        return citations.length - (j + 1);
+        return citations.length - (end + 1);
     }
 }
