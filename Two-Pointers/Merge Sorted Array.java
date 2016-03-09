3ee35af5ae79a3b1fbf5bb8358b707d0f9df433c
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2016-03-09 12:28:40 -0600

    Update Merge Sorted Array.java

--- Two-Pointers/Merge Sorted Array.java
+++ Two-Pointers/Merge Sorted Array.java
@@ -5,29 +5,23 @@
 //The number of elements initialized in nums1 and nums2 are m and n respectively.
 public class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i=m-1;
-        int j=n-1;
-        int k=m+n-1;
-        while(i>=0&&j>=0){
-            if(nums1[i]>=nums2[j]){
-                nums1[k]=nums1[i];
-                i--;
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p3 = m + n - 1;
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] >= nums2[p2]) {
+                nums1[p3] = nums1[p1];
+                p1--;
+            } else {
+                nums1[p3] = nums2[p2];
+                p2--;
             }
-            else{
-                nums1[k]=nums2[j];
-                j--;
-            }
-            k--;
+            p3--;
         }
-        while(i>=0){
-            nums1[k]=nums1[i];
-            i--;
-            k--;
-        }
-        while(j>=0){
-            nums1[k]=nums2[j];
-            j--;
-            k--;
+        while (p2 >= 0) {
+            nums1[p3] = nums2[p2];
+            p2--;
+            p3--;
         }
     }
 }
