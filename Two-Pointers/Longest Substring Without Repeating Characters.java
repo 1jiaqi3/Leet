766bfb1d6c7f9a778c1c17beb68978436e88995b
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2016-03-09 12:21:31 -0600

    Update Longest Substring Without Repeating Characters.java

--- Two-Pointers/Longest Substring Without Repeating Characters.java
+++ Two-Pointers/Longest Substring Without Repeating Characters.java
@@ -1,20 +1,17 @@
 //Given a string, find the length of the longest substring without repeating characters. 
 //For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. 
 //For "bbbbb" the longest substring is "b", with the length of 1.
-
-
-import java.util.*;
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
-        boolean[] exist=new boolean[256];
-		int i=0, maxLen=0;
-		for(int j=0;j<s.length();j++){
-			while(exist[s.charAt(j)]){
-			    exist[s.charAt(i)]=false;
-				i++;
+        boolean[] exist = new boolean[256];
+		int p1 = 0, maxLen = 0;
+		for (int p2 = 0; p2 < s.length(); p2++) {
+			while (exist[s.charAt(p2)]) {
+			    exist[s.charAt(p1)] = false;
+				p1++;
 			}
-			exist[s.charAt(j)]=true;
-			maxLen=Math.max(maxLen, j-i+1);
+			exist[s.charAt(p2)] = true;
+			maxLen = Math.max(maxLen, p2 - p1 + 1);
 		}
 		return maxLen;
     }
