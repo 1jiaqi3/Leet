29be2a79ba19dd70840415975ea924d45d43aa32
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2016-03-11 17:21:40 -0600

    Update Combination Sum.java

--- Backtracking/Combination Sum.java
+++ Backtracking/Combination Sum.java
@@ -1,29 +1,35 @@
-//Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+/*
+Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
 
-//The same repeated number may be chosen from C unlimited number of times.
+The same repeated number may be chosen from C unlimited number of times.
 
-//Note:
-//All numbers (including target) will be positive integers.
-//Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
-//The solution set must not contain duplicate combinations.
-//For example, given candidate set 2,3,6,7 and target 7, 
-//A solution set is: 
-//[7] 
-//[2, 2, 3] 
+Note:
+All numbers (including target) will be positive integers.
+Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+The solution set must not contain duplicate combinations.
+For example, given candidate set 2,3,6,7 and target 7, 
+A solution set is: 
+[7] 
+[2, 2, 3] 
+*/
 public class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> list=new ArrayList<>();
-        List<Integer> cur=new ArrayList<>();
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> cur = new ArrayList<>();
         Arrays.sort(candidates);
-        Backtracking(list,cur,target,candidates,0);
+        Backtracking(list, cur, target, candidates, 0);
         return list;
     }
-    public void Backtracking(List<List<Integer>> list,List<Integer> cur,int target,int[] candidates,int level){
-        if(target==0) list.add(new ArrayList(cur));
-        if(target<0) return;
-        for(int i=level;i<candidates.length;i++){
+    public void Backtracking(List<List<Integer>> list, List<Integer> cur, int target, int[] candidates, int level){
+        if (target == 0) {
+            list.add(new ArrayList(cur));
+        }
+        if(target < 0) {
+            return;
+        }
+        for(int i = level; i < candidates.length; i++){
             cur.add(candidates[i]);
-            Backtracking(list,cur,target-candidates[i],candidates,i);
+            Backtracking(list, cur, target - candidates[i], candidates, i);
             cur.remove(cur.size()-1);
         }
     }
