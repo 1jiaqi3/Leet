2efaf740f0047e3551b1bf7a330f5d282c8378a1
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2016-03-11 17:10:12 -0600

    Update Permutations.java

--- Backtracking/Permutations.java
+++ Backtracking/Permutations.java
@@ -1,33 +1,37 @@
-//Given a collection of distinct numbers, return all possible permutations.
+/*
+Given a collection of distinct numbers, return all possible permutations.
 
-//For example,
-//[1,2,3] have the following permutations:
-//[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
-
+For example,
+[1,2,3] have the following permutations:
+[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
+*/
 
 public class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> list=new ArrayList<>();
-        List<Integer> cur=new ArrayList<>();
-        ArrayList<Integer> numbers=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> cur = new ArrayList<>();
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
             numbers.add(nums[i]);
         }
-        DFS(list,cur,numbers);
+        DFS(list, 0, numbers);
         return list;
     }
-    public void DFS(List<List<Integer>> list,List<Integer> cur,ArrayList<Integer> numbers){
-        if(numbers.size()==0) list.add(new ArrayList<Integer> (cur));
-        for(int i=0;i<numbers.size();i++){
-            cur.add(numbers.get(i));
-            ArrayList<Integer> tmp;
-            if(numbers.size()>1){
-                tmp=new ArrayList<Integer>(numbers);
-                tmp.remove(i);
-            }
-            else tmp=new ArrayList<>();
-            DFS(list,cur,tmp);
-            cur.remove(cur.size()-1);
+    public void DFS(List<List<Integer>> list, int begin, ArrayList<Integer> numbers) {
+        if (begin >= numbers.size() - 1) {
+            ArrayList<Integer> newList = new ArrayList<>(numbers);
+            list.add(newList);
+            return;
         }
+        for (int i = begin; i < numbers.size(); i++) {
+            swap(numbers, begin, i);
+            DFS(list, begin + 1, numbers);
+            swap(numbers, begin, i);
+        }
+    }
+    public void swap(ArrayList<Integer> list, int i, int j) {
+        int temp = list.get(i);
+        list.set(i, list.get(j));
+        list.set(j, temp);
     }
 }
