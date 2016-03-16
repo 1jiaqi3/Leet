a5927d840b42a852a6b991bb0e59d40bb49ec294
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2016-03-16 16:41:00 -0500

    Create Rectangle Area.java

---
+++ Math/Rectangle Area.java
@@ -0,0 +1,22 @@
+/*
+Find the total area covered by two rectilinear rectangles in a 2D plane.
+
+Each rectangle is defined by its bottom left corner and top right corner as shown in the figure.
+
+Rectangle Area
+Assume that the total area is never beyond the maximum possible value of int.
+*/
+public class Solution {
+    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        int left = Math.max(A, E);
+        int right = Math.min(C, G);
+        int bottom = Math.max(B, F);
+        int top = Math.min(D, H);
+        int recArea = (C - A) * (D - B) + (G - E) * (H - F);
+        if (left >= right || bottom >= top) {
+            return recArea;
+        } else {
+            return recArea - (right - left) * (top - bottom);
+        }
+    }
+}
