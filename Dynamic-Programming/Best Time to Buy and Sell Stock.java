cd7932bb3e063945f5c98c96e9d694b32be334d2
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2016-03-18 12:52:06 -0500

    Update Best Time to Buy and Sell Stock.java

--- Dynamic-Programming/Best Time to Buy and Sell Stock.java
+++ Dynamic-Programming/Best Time to Buy and Sell Stock.java
@@ -4,13 +4,15 @@
 //design an algorithm to find the maximum profit.
 public class Solution {
     public int maxProfit(int[] prices) {
-        if(prices.length<2) return 0;
-        int min=prices[0];
-        int max=prices[1]-prices[0];
-        for(int i=2;i<prices.length;i++){
-            min=Math.min(min, prices[i-1]);
-            max=Math.max(max,prices[i]-min);
+        if (prices.length < 2) {
+            return 0;
         }
-        return Math.max(max,0);
+        int min = prices[0];
+        int max = prices[1] - prices[0];
+        for(int i = 2; i < prices.length; i++) {
+            min = Math.min(min, prices[i - 1]);
+            max = Math.max(max, prices[i] - min);
+        }
+        return Math.max(max, 0);
     }
 }
