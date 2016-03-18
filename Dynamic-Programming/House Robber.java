1a0c604c2abfcf32a848461d19ebf4ada04124ab
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2016-03-18 13:04:45 -0500

    Update House Robber.java

--- Dynamic-Programming/House Robber.java
+++ Dynamic-Programming/House Robber.java
@@ -1,22 +1,27 @@
-//You are a professional robber planning to rob houses along a street. 
-//Each house has a certain amount of money stashed, 
-//the only constraint stopping you from robbing each of them is that adjacent houses have security system connected 
-//and it will automatically contact the police if two adjacent houses were broken into on the same night.
-//Given a list of non-negative integers representing the amount of money of each house, 
-//determine the maximum amount of money you can rob tonight without alerting the police.
+/*
+You are a professional robber planning to rob houses along a street. 
+Each house has a certain amount of money stashed, 
+the only constraint stopping you from robbing each of them is that adjacent houses have security system connected 
+and it will automatically contact the police if two adjacent houses were broken into on the same night.
+Given a list of non-negative integers representing the amount of money of each house, 
+determine the maximum amount of money you can rob tonight without alerting the police.
+*/
 public class Solution {
     public int rob(int[] nums) {
-        int num=nums.length;
-        if(num==0) return 0;
-        int[] dp=new int[num+1];
-        dp[0]=0;
-        dp[1]=nums[0];
-        if(num==1) return dp[1];
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        if (nums.length == 1) {
+            return dp[1];
+        }
         else{
-            for(int i=2;i<=num;i++){
-                dp[i]=Math.max(dp[i-1],nums[i-1]+dp[i-2]);
+            for (int i = 2; i <= nums.length; i++) {
+                dp[i] = Math.max(dp[i - 1], nums[i - 1] + dp[i - 2]);
             }
         }
-        return dp[num];
+        return dp[nums.length];
     }
 }
