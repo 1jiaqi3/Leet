c3599d84efb9a9b985934a676462e12b2a563072
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2016-03-18 01:00:31 -0500

    Update Range Sum Query_Immutable.java

--- Dynamic-Programming/Range Sum Query_Immutable.java
+++ Dynamic-Programming/Range Sum Query_Immutable.java
@@ -12,17 +12,18 @@
 public class NumArray {
     private int[] dp;
     public NumArray(int[] nums) {
-        dp=new int[nums.length+1];
-        dp[0]=0;
-        for(int i=1;i<nums.length+1;i++){
-            dp[i]=dp[i-1]+nums[i-1];
+        dp = new int[nums.length + 1];
+        dp[0] = 0;
+        for (int i = 1; i < nums.length + 1; i++) {
+            dp[i] = dp[i - 1] + nums[i - 1];
         }
     }
 
     public int sumRange(int i, int j) {
-        return dp[j+1]-dp[i];
+        return dp[j + 1] - dp[i];
     }
 }
+
 
 
 // Your NumArray object will be instantiated and called as such:
