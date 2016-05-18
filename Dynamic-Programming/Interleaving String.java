52be2273dcbcbc9cb84cd9892beb553b1de618b0
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2016-05-18 23:38:50 +0800

    Create Interleaving String.java

---
+++ Dynamic-Programming/Interleaving String.java
@@ -0,0 +1,39 @@
+/*
+Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
+
+For example,
+Given:
+s1 = "aabcc",
+s2 = "dbbca",
+
+When s3 = "aadbbcbcac", return true.
+When s3 = "aadbbbaccc", return false.
+*/
+public class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s1.length() == 0) {
+            return s2.equals(s3);
+        } else if (s2.length() == 0) {
+            return s1.equals(s3);
+        }
+        if (s1.length() + s2.length() != s3.length()) {
+            return false;
+        }
+        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
+        for (int i = 0; i <= s1.length(); i++) {
+            for (int j = 0; j <= s2.length(); j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = true;
+                } else if (i == 0) {
+                    dp[i][j] = dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i - 1);
+                } else {
+                    dp[i][j] = dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1) 
+                    || dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1);
+                }
+            }
+        }
+        return dp[s1.length()][s2.length()];
+    }
+}
